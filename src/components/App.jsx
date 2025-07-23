b6f4829288d85812caaffc5dd3e74d4a6cc3c22e
RJECommissioning <molly.edwards@rjeglobal.com>
2025-07-23 13:08:23 +1000

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,48 @@
+import React from 'react';
+import { AppProvider } from './context/AppContext';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import StartNewProject from './pages/StartNewProject';
+import ContinueProject from './pages/ContinueProject';
+import MissingEquipment from './pages/MissingEquipment';
+import { useAppContext } from './context/AppContext';
+import './App.css';
 
+// Main App Content Component
+function AppContent() {
+  const { state } = useAppContext();
+  const currentPage = state.ui.current_page;
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'start-new-project':
+        return <StartNewProject />;
+      case 'continue-project':
+        return <ContinueProject />;
+      case 'missing-equipment':
+        return <MissingEquipment />;
+      case 'home':
+      default:
+        return <Home />;
+    }
+  };
+
+  return (
+    <Layout>
+      {renderPage()}
+    </Layout>
+  );
+}
+
+// Main App Component
+function App() {
+  return (
+    <div className="App">
+      <AppProvider>
+        <AppContent />
+      </AppProvider>
+    </div>
+  );
+}
+
+export default App;
