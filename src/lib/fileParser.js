cf4ed08d5c6ab2234bb57251fd5160cf38f8cc42
RJECommissioning <molly.edwards@rjeglobal.com>
2025-07-24 09:37:15 +1000

    Update fileParser.js

--- src/lib/fileParser.js
+++ src/lib/fileParser.js
@@ -148,20 +148,22 @@ const processEquipmentData = (equipment, originalHeaders) => {
         // Map common column variations to standard names
         const columnMappings = {
          const columnMappings = {
+           
+        // Map common column variations to standard names
+      const columnMappings = {
         'equipment_number': ['equipment_number', 'equipment_no', 'equipment_code', 'code', 'equipment', 'tag', 'id', 'asset_number', 'tag_number'],
         'description': ['description', 'desc', 'equipment_description', 'name', 'title', 'equipment_name', 'asset_description'],
         'plu_field': ['plu_field', 'plu', 'secondary_code', 'alt_code', 'alternative_code'],
         'commissioning_status': ['commissioning_status', 'status', 'commissioning', 'comm_status', 'commission_status', 'included'],
-        'parent_equipment_code': ['parent_equipment_code', 'parent_equipment', 'parent_code', 'parent_tag', 'parent', 'parent_equipment_number'], // KEY ADDITION!
+        'parent_equipment_code': ['parent_equipment_code', 'parent_equipment', 'parent_code', 'parent_tag', 'parent', 'parent_equipment_number'],
         'subsystem': ['subsystem', 'sub_system', 'system', 'sys'],
         'location': ['location', 'area', 'zone'],
         'category': ['category', 'cat', 'type', 'class'],
-        'parent_equipment_code': ['parent_equipment_code', 'parent_equipment', 'parent_code', 'parent_tag', 'parent', 'parent_equipment_number'],
         'manufacturer': ['manufacturer', 'make', 'vendor', 'supplier'],
         'model': ['model', 'model_number', 'part_number'],
         'voltage': ['voltage', 'volt', 'kv', 'rated_voltage'],
         'power': ['power', 'kw', 'mw', 'rating', 'capacity']
-      };
+      };;
 
         // Find matching columns for each standard field
         for (const [standardField, variations] of Object.entries(columnMappings)) {
